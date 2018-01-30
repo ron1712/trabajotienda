5eca837878328562528c419c4ca917d346dec64d
JhonPC <JhonPC@192.168.0.106>
2018-01-30 17:30:57 -0500

    Vista Nueva Tienda

--- trabajotienda/src/trabajotienda/rnegocio/vistas/FrmNuevaTienda.java
+++ trabajotienda/src/trabajotienda/rnegocio/vistas/FrmNuevaTienda.java
@@ -1,10 +1,16 @@
 package trabajotienda.rnegocio.vistas;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 import javax.swing.*;
 import java.util.*;
+import trabajotienda.rnegocio.dao.IPersona;
+import trabajotienda.rnegocio.dao.IProducto;
 import trabajotienda.rnegocio.entidades.Persona;
 import trabajotienda.rnegocio.entidades.Producto;
+import trabajotienda.rnegocio.impl.PersonaImp;
+import trabajotienda.rnegocio.impl.ProductoImp;
 
 public class FrmNuevaTienda extends JInternalFrame {
 
@@ -23,9 +29,14 @@ public class FrmNuevaTienda extends JInternalFrame {
     JTextField telefono;
 
     JComboBox<Persona> cbxPersonas;
+    List<Persona> lstPersonas;
     JComboBox<Producto> cbxProducto;
+    List<Producto> lstProductos;
 
-    JPanel pnlPrinc;
+    JButton btnIngresar;
+    JButton btnLimpiar;
+    JButton btnCancelar;
+
     JPanel pnlTienda;
     JPanel pnlBotones;
 
@@ -33,7 +44,7 @@ public class FrmNuevaTienda extends JInternalFrame {
         this.setLayout(new BorderLayout(15, 15));
 
         titulo = new JLabel("Nueva Tienda");
-        pnlPrinc.add(titulo, BorderLayout.NORTH);
+        this.add(titulo, BorderLayout.NORTH);
 
         txCodTienda = new JLabel("Codigo Tiemda");
         txPersona = new JLabel("Persona");
@@ -47,14 +58,58 @@ public class FrmNuevaTienda extends JInternalFrame {
         direccion = new JTextField("");
         telefono = new JTextField("");
 
-        cbxPersonas = new JComboBox<>();
-        cbxProducto = new JComboBox<>();
-        
-        
+        cargarPersonas();
+        cbxPersonas = new JComboBox(lstPersonas.toArray());
+        cargarProductos();
+        cbxProducto = new JComboBox(lstProductos.toArray());
+
+        btnIngresar = new JButton("Ingresar");
+        btnLimpiar = new JButton("Limpiar");
+        btnCancelar = new JButton("Cancelar");
+
+        pnlTienda = new JPanel(new GridLayout(6, 2));
+        pnlTienda.add(txCodTienda);
+        pnlTienda.add(codTienda);
+        pnlTienda.add(txNombre);
+        pnlTienda.add(nombre);
+        pnlTienda.add(txDireccion);
+        pnlTienda.add(direccion);
+        pnlTienda.add(txPersona);
+        pnlTienda.add(cbxPersonas);
+        pnlTienda.add(txProducto);
+        pnlTienda.add(cbxProducto);
+
+        pnlBotones = new JPanel(new GridLayout(1, 3));
+        pnlBotones.add(btnIngresar);
+        pnlBotones.add(btnLimpiar);
+        pnlBotones.add(btnCancelar);
+
+        this.add(pnlTienda, BorderLayout.CENTER);
+        this.add(pnlBotones, BorderLayout.SOUTH);
+        this.setSize(400,300);
+        this.setClosable(true);
 
     }
 
     public static void main(String[] args) {
-
+        FrmNuevaTienda frmMenu = new FrmNuevaTienda();
+        frmMenu.setVisible(true);
+    }
+    
+    public void cargarPersonas(){
+        IPersona personaDao = new PersonaImp();
+        try {
+            lstPersonas = personaDao.obtener();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "ERROR AL CARGAR CURSOS", "ERROR" + e.getMessage(), JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+    public void cargarProductos() {
+        IProducto productoDao = new ProductoImp();
+        try {
+            lstProductos = productoDao.obtener(1);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "ERROR AL CARGAR CURSOS", "ERROR" + e.getMessage(), JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
